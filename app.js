862633de6275e3a0993ed2a9dfb00c82d58b9fec
Rathtana Duong <drathtana@yahoo.com>
2019-05-06 14:36:57 -0700

    deployment

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const errorController = require('./controllers/error');
 const shopController = require('./controllers/shop');
 const isAuth = require('./middleware/is-auth');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://Rathtana:admin1@cluster0-bfxys.mongodb.net/shop?retryWrites=true{ useNewUrlParser: true }';
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-bfxys.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true{useNewUrlParser: true }`;
 
 
 const app = express();
@@ -48,10 +50,13 @@ const fileFilter = (req, file, cb) => {
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+app.use(helmet());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
@@ -121,7 +126,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
